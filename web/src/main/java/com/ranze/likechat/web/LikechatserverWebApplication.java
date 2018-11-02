122fa1b3982957255f76cfa5163441eb35502a17
ranze <luyong1107@gmail.com>
2018-11-02 21:39:49 +0800

    First commit

---
+++ web/src/main/java/com/ranze/likechat/web/LikechatserverWebApplication.java
@@ -0,0 +1,12 @@
+package com.ranze.likechat.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikechatserverWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikechatserverWebApplication.class, args);
+    }
+}
