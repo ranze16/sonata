503e1820ae130fc7ae2e72c15963780936c1cc39
ranze <luyong1107@gmail.com>
2018-11-08 00:58:30 +0800

    Add im module

---
+++ im/src/main/java/com/ranze/likechat/im/LikechatserverImApplication.java
@@ -0,0 +1,12 @@
+package com.ranze.likechat.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikechatserverImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikechatserverImApplication.class, args);
+    }
+}
